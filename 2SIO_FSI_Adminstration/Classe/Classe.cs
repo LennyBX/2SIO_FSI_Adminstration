bb5ec25869dd519d1bdbd83860417892e7e82a0f
lenny69200 <l.bondoux@lyon.ort.asso.fr>
2023-03-16 14:39:41 +0100

    Ajout classe et database

--- 2SIO_FSI_Adminstration/Classe/Classe.cs
+++ 2SIO_FSI_Adminstration/Classe/Classe.cs
@@ -8,5 +8,17 @@ namespace _2SIO_FSI_Adminstration.Classe
 {
     internal class Classe
     {
+        private int idClasse;
+        private string libelleClasse; 
+
+        public Classe(int idClasse, string libelleClasse)
+        {
+            this.idClasse = idClasse;
+            this.libelleClasse = libelleClasse;
+        }
+
+        public int IdClasse { get => idClasse; set => idClasse = value; }
+        public String LibelleClasse { get => libelleClasse; set => libelleClasse = value; }
+
     }
 }
--- 2SIO_FSI_Adminstration/Classe/Database.cs
+++ 2SIO_FSI_Adminstration/Classe/Database.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using Npgsql;
+using NpgsqlTypes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace _2SIO_FSI_Adminstration.Classe
 {
-    internal class Database
+    class Database
     {
+
+        String hostname = "localhost";
+        String port = "5432";
+        String database = "FSI_Gestion";
+        String user = "postgres";
+        String password = "pgadmin";
+
+        NpgsqlConnection npgsqlConnection; 
+
+        public Database()
+        {
+            String connectionString = "Server=" + hostname + ";Port=" + port + ";Database=" + database + ";User Id=" + user + ";Password=" + password + ";";
+            npgsqlConnection = new NpgsqlConnection(connectionString);
+            npgsqlConnection.Open();
+        }
+
+        public Array<Etudiant> getAllEtudiant()
+        {
+            NpgsqlCommand MyCmd = new NpgsqlCommand("SELECT * FROM :table;", npgsqlConnection);
+            MyCmd.Parameters.Add(new NpgsqlParameter("table", NpgsqlDbType.Varchar)).Value = table;
+            NpgsqlDataReader npgsqlDataReader = MyCmd.ExecuteReader();
+            if (dr.Read())
+            {
+                int idUti = dr.GetInt32(0);
+                loginUti = dr.GetString(1);
+                mdpUti = dr.GetString(2);
+                Utilisateur uti = new Utilisateur(idUti, loginUti, mdpUti);
+
+                //Ouverture du formulaire d'accueil si la connexion est ok
+                this.Hide();
+                Form formAccueil = new Accueil(uti);
+                formAccueil.Show();
+            }
+
+            return null;
+        }
+
     }
 }
